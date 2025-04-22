d7b242ff00d8dd9c1be1da0255642fab3d25d7d5
cozy-ito <cozy.ito.dev@gmail.com>
2025-04-22 15:06:16 +0900

    chore: 불필요한 콘솔 출력 제거

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,6 @@ const aliases = Object.entries(tsPathsConfig.compilerOptions.paths).map(
   }),
 );
 
-console.log(aliases);
-
 //* https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
