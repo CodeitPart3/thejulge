1f720ccd6a3d41a371bf9f58d9df39412d1e4745
오수빈 Subin OH <almighty5@snu.ac.kr>
2025-04-22 14:26:48 +0900

    feat: 정적 asset 추가

---
+++ src/assets/shop/index.ts
@@ -0,0 +1,19 @@
+import ShopEx1 from "./Image-light-1.svg";
+import ShopEx2 from "./Image-light-2.svg";
+import ShopEx3 from "./Image-light-3.svg";
+import ShopEx4 from "./Image-light-4.svg";
+import ShopEx5 from "./Image-light-5.svg";
+import ShopEx6 from "./Image-light-6.svg";
+import ShopEx7 from "./Image-light-7.svg";
+import ShopEx8 from "./Image-light.svg";
+
+export {
+  ShopEx1,
+  ShopEx2,
+  ShopEx3,
+  ShopEx4,
+  ShopEx5,
+  ShopEx6,
+  ShopEx7,
+  ShopEx8,
+};
