1a0b4ddb46899847ca3619cb59097957e89ec482
cozy-ito <cozy.ito.dev@gmail.com>
2025-04-24 11:23:34 +0900

    chore: vite-plugin-svgr 적용

--- src/assets/logo/index.ts
+++ src/assets/logo/index.ts
@@ -1,6 +1,6 @@
-import Envelope from "./envelope.svg";
-import Facebook from "./facebook.svg";
-import Instagram from "./instagram.svg";
-import Logo from "./thejulge.svg";
+import Envelope from "./envelope.svg?react";
+import Facebook from "./facebook.svg?react";
+import Instagram from "./instagram.svg?react";
+import Logo from "./thejulge.svg?react";
 
 export { Envelope, Facebook, Instagram, Logo };
