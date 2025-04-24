e2e4b7486e17d9b619048160b66e2cd366688f87
cozy-ito <cozy.ito.dev@gmail.com>
2025-04-24 11:15:03 +0900

    temp

--- src/pages/NoticeListPage.tsx
+++ src/pages/NoticeListPage.tsx
@@ -1,3 +1,12 @@
+import Input from "@/components/Input";
+
 export default function NoticeListPage() {
-  return <div>NoticeListPage</div>;
+  return (
+    <div>
+      NoticeListPage
+      <div>
+        <Input label="비밀번호" placeholder="입력" postfix={<span>원</span>} />
+      </div>
+    </div>
+  );
 }
