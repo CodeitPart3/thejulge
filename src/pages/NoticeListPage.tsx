7e67d4e1a0768765f1e81684d1f4199b0d339a75
cozy-ito <cozy.ito.dev@gmail.com>
2025-04-25 11:23:24 +0900

    feat: 컴포넌트 테스트 코드 제거

--- src/pages/NoticeListPage.tsx
+++ src/pages/NoticeListPage.tsx
@@ -1,12 +1,3 @@
-import Input from "@/components/Input";
-
 export default function NoticeListPage() {
-  return (
-    <div>
-      NoticeListPage
-      <div>
-        <Input label="비밀번호" placeholder="입력" postfix={<span>원</span>} />
-      </div>
-    </div>
-  );
+  return <div>NoticeListPage</div>;
 }
