c2992cd6e9830d7872900f346e379b909af10045
ghdtnals <hongsumin58@gmail.com>
2025-04-26 14:28:26 +0900

    feat: PostList 컴포넌트 생성

---
+++ src/components/Post/PostList.tsx
@@ -0,0 +1,28 @@
+import Post from "./Post";
+
+interface PostData {
+  id: string;
+  name: string;
+  imageUrl: string;
+  address1: string;
+  originalHourlyPay: number;
+  link: string;
+  hourlyPay: number;
+  startsAt: string;
+  workhour: number;
+  closed: boolean;
+}
+
+interface PostListProps {
+  posts: PostData[];
+}
+
+export default function PostList({ posts }: PostListProps) {
+  return (
+    <section className="grid grid-cols-2 xl:grid-cols-3 gap-x-2 gap-y-4 md:gap-x-[14px] md:gap-y-[32px]">
+      {posts.map((post) => (
+        <Post key={post.id} {...post} />
+      ))}
+    </section>
+  );
+}
