91914cbb285734467802c6670b921beef30a7f7e
ghdtnals <hongsumin58@gmail.com>
2025-04-28 17:52:34 +0900

    feat: Toast 컴폰넌트 구현

---
+++ src/components/Toast.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/utils/cn";
+
+interface ToastProps {
+  label: string;
+  className?: string;
+}
+
+export default function Toast({ label, className }: ToastProps) {
+  return (
+    <div
+      className={cn(
+        "inline-block rounded-md bg-red-30 px-4 py-[10px] text-white body1-regular",
+        className,
+      )}
+    >
+      {label}
+    </div>
+  );
+}
