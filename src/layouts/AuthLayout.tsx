6bfce2d94f18c938ab831b61b69786f4ca853dab
ghdtnals <hongsumin58@gmail.com>
2025-05-07 17:12:11 +0900

    fix: AuthLayout 수정

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
   return (
-    <div className="min-h-screen flex justify-center items-start pt-[8.75rem] sm:pt-[17.5rem] lg:pt-[19.5rem] pb-[18.75rem] sm:pb-[27.625rem] lg:pb-[17rem]">
+    <div className="min-h-screen flex justify-center items-start pt-[8.75rem] sm:pt-[17.5rem]">
       <div className="w-[21.875rem]">
         <Outlet />
       </div>
